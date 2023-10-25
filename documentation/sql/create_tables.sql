b7f60ab700926063c1d3d41a7c7a9ba3697f628b
floatingeye <eriklougee@gmail.com>
2023-10-25 15:42:14 -0400

    Adding docker support - Created very bare bones Dockerfile - Updated mysql script to create the database and corresponding tables

--- documentation/sql/create_tables.sql
+++ documentation/sql/create_tables.sql
@@ -1,10 +1,30 @@
-CREATE TABLE Persona (
-  PersonaID INT UNSIGNED AUTOINCREMENT ,
-  CharacterClassID INT,
-  CurrentHP INT,
-  MaxHP INT,
-  Name VARCHAR(100),
-  XPos INT,
-  YPos INT,
-  PRIMARY KEY (PersonaID)
-)
+CREATE DATABASE game_engine;
+USE game_engine;
+
+CREATE TABLE persona (
+  persona_id INT AUTO_INCREMENT,
+  character_class_id INT,
+  current_hp INT,
+  maximum_hp INT,
+  name VARCHAR(100),
+  x_pos INT,
+  y_pos INT,
+  PRIMARY KEY (persona_id)
+);
+
+CREATE TABLE ability (
+  ability_id INT AUTO_INCREMENT,
+  name VARCHAR(100),
+  description TEXT,
+  hp_drained INT,
+  hp_restored INT,
+  hp_protected INT,
+  PRIMARY KEY (ability_id)
+);
+
+CREATE TABLE character_class (
+  character_class_id INT AUTO_INCREMENT,
+  name VARCHAR(100),
+  description TEXT,
+  PRIMARY KEY (character_class_id)
+);
